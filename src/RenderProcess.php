bacddecfc8e0b32f2147ff5afec24b5583993ee0
yf <291323003@qq.com>
2019-05-23 15:09:05 +0800

    init

--- src/RenderProcess.php
+++ src/RenderProcess.php
@@ -11,7 +11,33 @@ class RenderProcess extends AbstractProcess
     public function run($arg)
     {
         /** @var Config $arg  */
-
+        $sockFile = $arg->getTempDir()."/{$this->getProcessName()}.sock";
+        if (file_exists($sockFile))
+        {
+            unlink($sockFile);
+        }
+        $ctx = stream_context_create(['socket' => ['so_reuseaddr' => true, 'backlog' => 2048]]);
+        $socket = stream_socket_server("unix://$sockFile", $errno, $errStr,STREAM_SERVER_BIND | STREAM_SERVER_LISTEN,$ctx);
+        if (!$socket)
+        {
+            trigger_error($errStr);
+            return;
+        }
+        while (1){
+            $reply = null;
+            $conn = stream_socket_accept($socket,-1);
+            if($conn){
+                stream_set_timeout($conn,2);
+                $header = fread($conn,4);
+                $allLength = Protocol::packDataLength($header);
+                $data = fread($conn,$allLength );
+                if(strlen($data) == $allLength){
+                    $data = unserialize($data);
+                }
+            }
+            fwrite($conn,Protocol::pack(serialize($reply)));
+            fclose($conn);
+        }
     }
 
     public function onShutDown()
