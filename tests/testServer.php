1e4357f9f1f4aee3678d2ea05ef990be4268b9d8
叶华 <799475742@qq.com>
2020-03-19 20:54:58 +0800

    update testServer error

--- tests/testServer.php
+++ tests/testServer.php
@@ -11,43 +11,46 @@ use Swoole\Http\Request;
 use Swoole\Http\Response;
 
 require_once '../vendor/autoload.php';
+try {
+    // 命令行启动对应的模板引擎服务器
+    $param_arr = getopt('e:');
+    if (!isset($param_arr['e'])) {
+        echo "\ncurrent support engine: 'blade' 'think' 'plates' 'smarty'\nuse command : 'php testServer.php -e {engineName}' to start choose engine server\n\n";
+        exit;
+    }
 
-// 命令行启动对应的模板引擎服务器
-$param_arr = getopt('e:');
-if (!isset($param_arr['e'])) {
-    echo "\ncurrent support engine: 'blade' 'think' 'plates' 'smarty'\nuse command : 'php testServer.php -e {engineName}' to start choose engine server\n\n";
-    exit;
+    // 实例化模板引擎
+    $name = $param_arr['e'];
+    $engine = Smarty::class;
+    $cacheDir = __DIR__. '/TemplateCache';
+    $viewsDir = __DIR__. '/TemplateViews';
+    $engines = ['blade' => Blade::class, 'think' => Think::class, 'smarty' => Smarty::class, 'plates' => Plates::class];
+    $templates = ['blade' => 'blade', 'think' => 'think', 'smarty' => 'smarty.tpl', 'plates' => 'plates'];
+    if (array_key_exists($name, $engines)) {
+        $engine = $engines[$name];
+    } else {
+        echo "\nnot supported engine: {$name}\n\n";
+        exit;
+    }
+
+    /** @var RenderInterface $engine */
+    $engine = new $engine($viewsDir, $cacheDir);
+    $config = new Config;
+    $config->setRender($engine);
+    $render = new Render($config);
+
+    // 启动服务
+    $http = new swoole_http_server("0.0.0.0", 9501);
+    $http->on("request", function (Request $request, Response $response) use ($render, $templates, $name) {
+        $response->end($render->render($templates[$name], [
+            'engine' => $name,
+            'time' => date('Y-m-d H:i:s')
+        ]));
+    });
+
+    $render->attachServer($http);
+    echo "\ntemplate engine: {$param_arr['e']}\nlisten at: http://0.0.0.0:9501\n";
+    $http->start();
+}catch (\Throwable $e){
+    var_dump($e->getMessage());
 }
-
-// 实例化模板引擎
-$name = $param_arr['e'];
-$engine = Smarty::class;
-$cacheDir = dirname(__FILE__) . '/TemplateCache';
-$viewsDir = dirname(__FILE__) . '/TemplateViews';
-$engines = ['blade' => Blade::class, 'think' => Think::class, 'smarty' => Smarty::class, 'plates' => Plates::class];
-$templates = ['blade' => 'blade', 'think' => 'think', 'smarty' => 'smarty.tpl', 'plates' => 'plates'];
-if (array_key_exists($name, $engines)) {
-    $engine = $engines[$name];
-} else {
-    echo "\nnot supported engine: {$name}\n\n";
-    exit;
-}
-
-/** @var RenderInterface $engine */
-$engine = new $engine($viewsDir, $cacheDir);
-$config = new Config;
-$config->setRender($engine);
-$render = new Render($config);
-
-// 启动服务
-$http = new swoole_http_server("0.0.0.0", 9501);
-$http->on("request", function (Request $request, Response $response) use ($engine, $templates, $name) {
-    $response->end($engine->render($templates[$name], [
-        'engine' => $name,
-        'time' => date('Y-m-d H:i:s')
-    ]));
-});
-
-$render->attachServer($http);
-echo "\ntemplate engine: {$param_arr['e']}\nlisten at: http://0.0.0.0:9501\n";
-$http->start();
